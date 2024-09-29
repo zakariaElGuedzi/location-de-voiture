1e3bf068bdd8b2575df30bad6f5eb1a9ad8890e7
Adnane <56194677+AdnaneBennani@users.noreply.github.com>
2024-09-29 22:03:37 +0100

    Update Marque.php

--- Admin/Marque.php
+++ Admin/Marque.php
@@ -4,14 +4,14 @@ include "includes/navbar.php";
 <main class="content">
 			<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center py-4">
 				<div class="d-block mb-4 mb-md-0">
-					<h2 class="h4">Liste Vendeure</h2>
+					<h2 class="h4">Liste Marques</h2>
 				</div>
 				<div class="btn-toolbar mb-2 mb-md-0">
 					<a href="AjouterOperateur.php" class="btn btn-sm btn-gray-800 d-inline-flex align-items-center">
 						<svg class="icon icon-xs me-2" fill="none" stroke="currentColor" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
 						</svg>
-						Ajouter Vendeure
+						Ajouter Marque
 					</a>
 				</div>
 			</div>
