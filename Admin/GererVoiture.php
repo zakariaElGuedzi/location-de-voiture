64df66212ba93ecb5e63700f5456ae3850bf8e99
Adnane <56194677+AdnaneBennani@users.noreply.github.com>
2024-10-07 19:14:48 +0100

    Update GererVoiture.php

--- Admin/GererVoiture.php
+++ Admin/GererVoiture.php
@@ -26,7 +26,7 @@ $cars = $pdo->query('SELECT * FROM cars')->fetchAll(PDO::FETCH_OBJ);
 						<svg class="icon icon-xs me-2" fill="none" stroke="currentColor" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
 							<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
 						</svg>
-						Ajouter Vendeure
+						Ajouter Voiture
 					</a>
 				</div>
 			</div>
