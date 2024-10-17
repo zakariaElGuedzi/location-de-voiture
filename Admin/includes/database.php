42602d64ac445b00c26f4930f90374787b493613
Adnane <56194677+AdnaneBennani@users.noreply.github.com>
2024-10-18 00:49:34 +0100

    Update

--- Admin/includes/database.php
+++ Admin/includes/database.php
@@ -2,6 +2,8 @@
 ob_start();
 try{
   $pdo = new PDO('mysql:host=localhost;dbname=u674441585_locvoi','u674441585_PerlaPlaya','PerlaPlayaPlayaPerla12345!@#');
+  // $pdo = new PDO('mysql:host=localhost;dbname=locvoi','root','');
+
 }catch(PDOException $e){
   echo 'ERROR: ' . $e->getMessage();
   die();
